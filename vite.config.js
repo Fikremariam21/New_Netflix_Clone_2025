7208a53fa3a82f62158ffb78a570388d2cc8a0d9
Fikremariam Mekonnen <168227990+Fikremariam21@users.noreply.github.com>
2025-02-13 02:52:29 +0300

    Deployment

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/New_Netflix_Clone_2025/',
-  build: {
-    outDir: "docs"
-  },
+  base: '/New_Netflix_Clone_2025/'
 }); 
